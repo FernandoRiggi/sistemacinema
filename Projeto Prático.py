13db740214214fa7c3176dea7c7bf4555d5d695d
FernandoRiggi <Fernando_De_Riggi@outlook.com>
2024-05-16 21:17:13 -0300

    Projeto Prático.py

--- Projeto Prático.py
+++ Projeto Prático.py
@@ -1,13 +1,69 @@
-def incluir_elementos_filme(FILMES): #submenu para adicionar elementos no filme
+def add_codigo(FILMES):
+    codigo = input("Digite o código do filme: ")
+    if codigo not in FILMES:
+        FILMES[codigo]={}
+        with open("filmes.txt", "a") as file:
+            file.write(f"Código: {codigo}\n")
+    else:
+        print("O código já existe.")
+    return codigo
+def incluir_elementos_filme(FILMES, codigo): #submenu para adicionar elementos no filme
     print("Escolha os elementos que deseja adicionar")
-    print("1. Código do Filme")
-    print("2. Nome do Filme")
-    print("3. Ano de lançamento do Filme")
-    print("4. Diretor do Filme")
-    print("5. Atores do Filme")
-    print("6. Sair")
+    print("1. Nome do Filme")
+    print("2. Ano de lançamento do Filme")
+    print("3. Diretor do Filme")
+    print("4. Atores do Filme")
+    print("5. Sair")
     op=int(input("Escolha a opção que deseja realizar: "))
+    if op ==1:
+        nome = input("Digite o nome do filme: ")
+        FILMES[codigo]["Nome"]=nome
+        with open("filmes.txt", "a") as file:
+            file.write(f"Nome: {nome}\n")
+    elif op ==2:
+        ano=input("Digite o ano de lançamento do filme: ")
+        FILMES[codigo]["Ano"]=ano
+        with open("filmes.txt", "a") as file:
+            file.write(f"Ano: {ano}\n")
+    elif op==3:
+        diretor=input("Digite o nome do diretor: ")
+        FILMES[codigo]["Diretor"]=diretor
+        with open("filmes.txt", "a") as file:
+            file.write(f"Diretor: {diretor}\n")
+    elif op==4:
+        atores=input("Digite os atores, (para separar utilize vírgula): ")
+        FILMES[codigo]["Atores"]=atores.split(",")
+        with open("filmes.txt", "a") as file:
+            file.write(f"Atores: {",".join(atores.split(","))}\n")
+    elif op==5:
+        print("Saindo do menu de inclusão de elementos.")
+    else:
+        print("Operação inválida, escolha uma opção entre 1 e 5.")
     return op
+def listar_elemento_especifico(FILMES, codigo):
+    print("Escolha o elemento que deseja visualizar")
+    print("1. Nome do Filme")
+    print("2. Ano de lançamento do Filme")
+    print("3. Diretor do Filme")
+    print("4. Atores do Filme")
+    print("5. Sair")
+    op = int(input("Escolha a opção que deseja realizar: "))
+
+    if codigo in FILMES:
+        if op == 1:
+            print(f"Nome: {FILMES[codigo].get('Nome', 'Não disponível')}")
+        elif op == 2:
+            print(f"Ano: {FILMES[codigo].get('Ano', 'Não disponível')}")
+        elif op == 3:
+            print(f"Diretor: {FILMES[codigo].get('Diretor', 'Não disponível')}")
+        elif op == 4:
+            print(f"Atores: {', '.join(FILMES[codigo].get('Atores', ['Não disponível']))}")
+        elif op == 5:
+            print("Saindo do menu de listagem de elementos.")
+        else:
+            print("Operação inválida, escolha uma opção entre 1 e 5.")
+    else:
+        print("Código do filme não encontrado.")
 def submenu_filmes(FILMES): #submenu de filmes
     print("Escolha uma opção entre 1 à 5 ou digite 6 para sair")
     print("1. Listar todos os filmes")
@@ -40,12 +96,30 @@ def main(): #programa principal
                 if opfilmes ==1:
                     print(f"A lista de todos os filmes são: {FILMES}")
                 elif opfilmes==2:
+                    codigo = input("Digite o código do filme para listar os detalhes: ")
+                    listar_elemento_especifico(FILMES, codigo)
                 elif opfilmes==3:
-                    incluir_elementos_filme = incluir_elementos_filme(FILMES)
+                    codigo = add_codigo(FILMES)
+                    incluir_elementos_filme_op=1
+                    while incluir_elementos_filme_op !=5:
+                        incluir_elementos_filme_op = incluir_elementos_filme(FILMES, codigo)
                 elif opfilmes==4:
+                    codigo = input("Digite o código do filme que deseja alterar: ")
+                    if codigo in FILMES:
+                        incluir_elementos_filme_op=1
+                        while incluir_elementos_filme_op!=5:
+                            incluir_elementos_filme_op=incluir_elementos_filme(FILMES, codigo)
+                    else:
+                        print("Código não encontrado.")
                 elif opfilmes==5:
+                    codigo = input("Digite o código do filme que deseja excluir: ")
+                    if codigo in FILMES:
+                        del FILMES[codigo]
+                        print(f"Filme {codigo} excluído. \n")
+                    else: 
+                        print("Código do filme não encontrado")
                 elif opfilmes==6:
-                    print("Encerrando programa")
+                    print("Encerrando submenu de filmes.")
                 else:
                     print("Operação inválida, escolha uma opção entre 1 à 6.")
         elif operacao ==3: #abrirá o submenu de Sessões
