6e7a00a515fdcc379223007f7f2c90f1c740b611
FernandoRiggi <Fernando_De_Riggi@outlook.com>
2024-05-23 11:18:25 -0300

    5 commit

--- Projeto Prático.py
+++ Projeto Prático.py
@@ -1,34 +1,47 @@
+
 ##### SUBMENU DE RELÁTORIOS ######
 def submenu_relatorios(RELATORIOS):
-    print("Escolha uma opção entre 1 e 3, ou digite 4 para sair")
-    print("1. Mostrar todos os dados de todas as salas com parâmetros")
-    print("2. Mostrar todos os filmes lançados a partir de ano de lançamento específico")
-    print("3. Mostrar os elementos, a partir de uma data inicial até uma data final")
-    print("4. Sair")
+    print("Submenu RELATÓRIOS:")
+    print("   1. Mostrar todos os dados de todas as salas com parâmetros")
+    print("   2. Mostrar todos os filmes lançados a partir de ano de lançamento específico")
+    print("   3. Mostrar os elementos, a partir de uma data inicial até uma data final")
+    print("   4. Sair")
     op=int(input("Escolha a opção que deseja realizar: "))
     return op
 ####### SUBMENU DE SESSÕES #######
 def submenu_sessoes(SESSOES):
-    print("Escolha uma opção entre 1 e 5, ou digite 6 para sair")
-    print("1. Listar todas as salas")
-    print("2. Listar um elemento específico do conjunto")
-    print("3. Incluir elementos específico do conjunto")
-    print("4. Alterar elementos da sessão")
-    print("5. Excluir um elemento do conjunto das sessões")
-    print("6. Sair")
+    print("Submenu SESSÕES:")
+    print("   1. Listar todas as salas")
+    print("   2. Listar um elemento específico do conjunto")
+    print("   3. Incluir elementos específico do conjunto")
+    print("   4. Alterar elementos da sessão")
+    print("   5. Excluir um elemento do conjunto das sessões")
+    print("   6. Sair")
     op=int(input("Escolha a opção que deseja realizar: "))
     return op
 ###### SUBMENU DE SALAS #######
 def submenu_salas(SALAS):
-    print("Escolha uma opção entre 1 e 5 ou digite 6 para sair")
-    print("1. Listar todas as salas")
-    print("2. Listar um elemento específico do conjunto")
-    print("3. Incluir elementos específico do conjunto")
-    print("4. Alterar elementos da sala")
-    print("5. Excluir um elemento do conjunto das salas")
-    print("6. Sair")
+    print("Submenu SALAS:")
+    print("   1. Listar todas as salas")
+    print("   2. Listar um elemento específico do conjunto")
+    print("   3. Incluir elementos específico do conjunto")
+    print("   4. Alterar elementos da sala")
+    print("   5. Excluir um elemento do conjunto das salas")
+    print("   6. Sair")
     op=int(input("Escolha a opção que deseja realizar: "))
     return op
+######FUNÇÃO PARA REMOVER O FILME #############
+def remove_filme(FILMES, codigo):
+    if codigo in FILMES:  
+        print("Deseja confirmar a remoção do filme?")
+        confirmacao = input("Para confirmar digite o codigo do filme: ")
+        if confirmacao == codigo:
+            del FILMES[codigo]
+            return True
+        else:
+            print("Código de confirmação incorreto, retornando ao submenu FILMES")
+    else:
+        print("Código do filme não encontrado, retornando ao submenu FILMES")
 ######### FUNÇÃO PARA ADICIONAR O CODIGO DO FILME ########
 def add_codigo(FILMES):
     codigo = input("Digite o código do filme: ")
@@ -100,40 +113,47 @@ def listar_elemento_especifico(FILMES, codigo):
         print("Código do filme não encontrado.")
 ####### SUBMENU DE FILMES #######
 def submenu_filmes(FILMES): #submenu de filmes
-    print("Escolha uma opção entre 1 à 5 ou digite 6 para sair")
-    print("1. Listar todos os filmes")
-    print("2. Listar um elemento específico do conjunto")
-    print("3. Incluir elementos do filme")
-    print("4. Alterar elementos do filme")
-    print("5. Excluir um elemento do conjunto do filme")
-    print("6. Sair")
+    print("Submenu FILMES:")
+    print("   1. Listar todos os filmes")
+    print("   2. Listar um elemento específico do conjunto")
+    print("   3. Incluir elementos do filme")
+    print("   4. Alterar elementos do filme")
+    print("   5. Excluir um elemento do conjunto do filme")
+    print("   6. Sair")
     op = int(input("Escolha a opção que deseja realizar: "))
     return op
 #########MENU DE SUBOPÇÕES######
 def menu(): 
-    print("Escolha uma opção entre 1 à 4 ou digite 5 para sair")
-    print("1. Submenu de Salas")
-    print("2. Submenu de Filmes")
-    print("3. Submenu de Sessões")
-    print("4. Submenu de Relatórios")
-    print("5. Sair")
+    print("Menu de Opções:")
+    print("  1. Submenu de Salas")
+    print("  2. Submenu de Filmes")
+    print("  3. Submenu de Sessões")
+    print("  4. Submenu de Relatórios")
+    print("  5. Sair")
     op=int(input("Escolha a opção que deseja realizar: "))
     return op
 def main(): #programa principal
-    operacao=1
     FILMES={}
     SALAS={}
     SESSOES={}
     RELATORIOS={}
-    while operacao !=5:
+    Menu = True
+    while Menu:
         operacao = menu() #chama o menu das opções principais
         if operacao == 1: #abrirá o submenu de Salas
             opsalas = 1
-            while opsalas!=6:
+            Menu_salas = True
+            while Menu_salas == True:
                 opsalas = submenu_salas(SALAS) #chama o submenu de salas
+                if opsalas==1:
+                    print(f"A lista de salas são: {SALAS}")
+                elif opsalas ==6:
+                    print("Encerrando submenu de salas")
+                    Menu_salas = False
         elif operacao ==2: #abrirá o submenu de Filmes
             opfilmes=1
-            while opfilmes !=6:
+            Menu_filmes = True
+            while Menu_filmes == True:
                 opfilmes = submenu_filmes(FILMES) #chama o submenu de filmes
                 if opfilmes == 1:
                     print(f"A lista de filmes são: {FILMES}")
@@ -155,25 +175,37 @@ def main(): #programa principal
                         print("Código não encontrado.")
                 elif opfilmes==5:
                     codigo = input("Digite o código do filme que deseja excluir: ")
-                    if codigo in FILMES:
-                        del FILMES[codigo]
-                        print(f"Filme {codigo} excluído. \n")
-                    else: 
-                        print("Código do filme não encontrado")
+                    remover = remove_filme(FILMES, codigo)
+                    if remover == True:
+                        print(f"O filme {codigo} foi excluido")
                 elif opfilmes==6:
                     print("Encerrando submenu de filmes.")
+                    Menu_filmes=False
                 else:
                     print("Operação inválida, escolha uma opção entre 1 à 6.")
         elif operacao ==3: #abrirá o submenu de Sessões
             opsessoes = 1
-            while opsessoes !=6:
+            Menu_sessoes = True
+            while Menu_sessoes ==True:
                 opsessoes = submenu_sessoes(SESSOES) #chama o submenu de sessoes
+                if opsessoes == 1:
+                    print(f"A lista de todas as Sessões são: {SESSOES}")
+                elif opsessoes==6:
+                    print("Encerrando submenu de sessões.")
+                    Menu_sessoes=False
         elif operacao == 4: #abrirá o submenu de Relátorios
             oprelatorios = 1
-            while oprelatorios !=4:
+            Menu_relatorios = True
+            while Menu_relatorios==True:
                 oprelatorios = submenu_relatorios(RELATORIOS)
+                if oprelatorios==1:
+                    print("Mostrar todos as salas cujo tipo de exibição seja X e capacidade para mais de Y pessoas, onde X e Y são fornecidos pelo usuário;")
+                else:
+                    print("Encerrando submenu de relátorios")
+                    Menu_relatorios=False
         elif operacao==5: #encerrará o programa
             print("Encerrando programa")
+            Menu = False
         else: #caso digitado um número que não corresponde as operações válidas
             print("Operação inválida, escolha uma opção entre 1 à 5.")
 main()
